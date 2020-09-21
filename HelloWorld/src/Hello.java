b33c29c76340024bac82e92d6a081e77f4309c76
mwangicyrus <cyrus@tequalsystems.co.ke>
2020-09-21 12:12:33 +0300

    added the first lesson

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main (String [] args) {
+        System.out.println("Good morning Cyrus, Its a great day today");
+        int myFirstNumber =(10+5)*(2+7);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+    }
+}
